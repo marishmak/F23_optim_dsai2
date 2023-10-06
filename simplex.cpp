c984909d1a736f5560bf0ad223ddab49fd562261
Iurii <i.podkorytov@innopolis.university>
2023-10-06 12:53:31 +0300

    Moved examples to a separate file Translated comments; codestyle

--- simplex.cpp
+++ simplex.cpp
@@ -1,59 +1,14 @@
-/*
-example:
-1
-3 4
-2 1 -3 5
-1 2 2 4
-2 -1 1 2
-4 -2 1 -1
-40 8 10
-2
-
----Answer:41---
-
-
-example 2:
-1
-3 3
-9 10 16
-18 15 12
-6 4 8
-5 3 3
-360 132 180
-5
-
----Answer:300---
-
-
-1
-2 4
-6 8 5 9
-2 1 1 3
-1 3 1 2
-5 3
-2
-
----Answer:17---
-
-1
-3 2
-2 12
-2 4
-1 6
-1 3
-16 12 12
-2
-*/
-#include <iostream>
 #include <algorithm>
+#include <cmath>
 #include <iomanip>
-#include <vector>
+#include <iostream>
 #include <map>
+#include <vector>
 
 using namespace std;
 
-void printMatrix(vector <vector <double>>& matrix, vector<int>& variables) {
-    // печатает таблицу симплекс метода с 5 знаками после запятой, выделяя под каждый элемент таблицы по 10 символов
+void printMatrix(vector<vector<double>> &matrix, vector<int> &variables) {
+    // outputs simplex tableu with 5 decimal places
     int n = matrix.size();
     int m = matrix[0].size();
     for (int i = 0; i < n; i++) {
@@ -69,39 +24,34 @@ void printMatrix(vector <vector <double>>& matrix, vector<int>& variables) {
 }
 
 double getWithAccuracy(double n, long long accuracy) {
-    // accuracy - это степень 10. Чтобы обнулить все знаки, например, после второго, нужно исходное число умножить на 10^2 записать значение в целочисленную переменную.
-    // Потом, чтобы вернуться к исходной мантиссе, делим число на 10^2
+    // rounds number to log_10(accuracy) decimals
     long long temp = round(n * accuracy);
     return double(temp) / accuracy;
 }
 
-vector <vector <double>> getMatrix(long long& accuracy) {
+vector<vector<double>> getMatrix(long long &accuracy) {
     bool minMaxFlag;
     cout << "Enter \"0\" for minimization or \"1\" for maximization\n";
     cin >> minMaxFlag;
 
-    int n, m;
-    // n - количество ограничений (кол-во строк)
-    // m - количество переменных (кол-во столбцов)
-    cout << "Enter number of restrictions in linear programming problem\n";
+    int n, m;  // n - number of constraints (number of rows in A); m - number of
+               // varibles (number of colums in A)
+    cout << "Enter number of constrains in linear programming problem\n";
     cin >> n;
     cout << "Enter number of variables in linear programming problem\n";
     cin >> m;
 
-    vector <double> c(m);
-    cout << "Enter vector c, such that c^t*X (X is vector of all variables)-> ";
-    if (minMaxFlag) {
-        cout << "max\n";
-    }
-    else {
-        cout << "min\n";
-    }
+    vector<double> c(m);
+    cout << "Enter vector c, such that c^t*X (X is vector of all variables) is "
+            "the objective function ";
+
     for (int i = 0; i < m; i++) {
         cin >> c[i];
-        c[i] *= (minMaxFlag ? 1 : -1); //изменение целевой функции в соответствии с типом задачи (max/min)
+        c[i] *= (minMaxFlag ? 1 : -1);  // change the objective function
+                                        // according to the problem (max/min)
     }
     cout << "Enter matrix A, such that AX <= b\n";
-    vector <vector<double>> a(n);
+    vector<vector<double>> a(n);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             double x;
@@ -110,18 +60,21 @@ vector <vector <double>> getMatrix(long long& accuracy) {
         }
     }
     cout << "Enter vector b, such that AX <= b\n";
-    vector <double> b(n);
+    vector<double> b(n);
     for (int i = 0; i < n; i++) {
         cin >> b[i];
     }
 
-    cout << "Enter the maximum number of decimal places after which everything will be reset to zero (0 - 10).\n";
+    cout << "Enter the maximum number of decimal places after which everything "
+            "will be reset to zero (0 - 10).\n";
     cin >> accuracy;
-    accuracy = pow(10, accuracy); // получение степени 10, необходимой для последующих округлений
+    accuracy = pow(10, accuracy);
 
-    vector <double> temp(n + m + 1); // vector из нулей, которым заполняется каждая строка таблицы
-    vector <vector <double>> matrix(n + 1, temp);
-    for (int i = 0; i < m; i++) { //заполнение таблицы в соответствии с презентацией
+    vector<double> temp(
+        n + m + 1);  // a vector filled with zeros for table construction
+    vector<vector<double>> matrix(n + 1, temp);
+    // filling the table
+    for (int i = 0; i < m; i++) {
         matrix[0][i] = -getWithAccuracy(c[i], accuracy);
     }
     for (int i = 1; i < n + 1; i++) {
@@ -133,8 +86,7 @@ vector <vector <double>> getMatrix(long long& accuracy) {
         for (int j = m; j < n + m + 1; j++) {
             if (i == j - m + 1) {
                 matrix[i][j] = 1;
-            }
-            else {
+            } else {
                 matrix[i][j] = 0;
             }
         }
@@ -145,38 +97,43 @@ vector <vector <double>> getMatrix(long long& accuracy) {
     return matrix;
 }
 
-map<int, double> simplex(vector<vector<double>> simplexMatrix, long long accuracy) {
+map<int, double> simplex(vector<vector<double>> simplexMatrix,
+                         long long accuracy) {
     cout << "The iterations of the algorithm look like this:\n";
     int n = simplexMatrix[0].size();
     int m = simplexMatrix.size();
-    vector <int> variables(m); // хранит индексы переменных базиса
+    vector<int> variables(m);  // for storing indecies of basic variables
     variables[0] = 0;
     for (int i = n - m + 1; i < n; i++) {
         variables[i - n + m] = i;
     }
-    //selecting var to proceed
+    // selecting var to proceed
     while (true) {
         printMatrix(simplexMatrix, variables);
         cout << '\n';
-        double minVal = simplexMatrix[0][0]; // Assume the first element is the minimum
-        int minPos = 0;     // Position of the minimum value
-        // Iterate through the array to find the minimum value and its position
+        double minVal =
+            simplexMatrix[0][0];  // assume the first element is the minimum
+        int minPos = 0;           // position of the minimum value
+        // iterate through the array to find the minimum value and its position
         for (int i = 1; i < n - 1; i++) {
             if (simplexMatrix[0][i] < minVal) {
                 minVal = simplexMatrix[0][i];
                 minPos = i;
             }
         }
-        //проверяем на то, что нам не нужно дальше делать шаги
+        // check if the problem is solved
         if (minVal >= 0) {
             break;
         }
-        //находим pivot element
+        // finding pivot element
         double minRatio = -1;
         double minRatioVal = 0;
         int minRatioPos = -1;
         for (int j = 1; j < m; j++) {
-            if ((simplexMatrix[j][n - 1] / simplexMatrix[j][minPos] < minRatio || minRatioPos == -1) && (simplexMatrix[j][n - 1] / simplexMatrix[j][minPos]) > 0) {
+            if ((simplexMatrix[j][n - 1] / simplexMatrix[j][minPos] <
+                     minRatio ||
+                 minRatioPos == -1) &&
+                (simplexMatrix[j][n - 1] / simplexMatrix[j][minPos]) > 0) {
                 minRatio = simplexMatrix[j][n - 1] / simplexMatrix[j][minPos];
                 minRatioPos = j;
                 minRatioVal = simplexMatrix[j][minPos];
@@ -184,20 +141,23 @@ map<int, double> simplex(vector<vector<double>> simplexMatrix, long long accurac
         }
         if (minRatio == -1) {
             return {};
-        }
-        else {
+        } else {
             variables[minRatioPos] = minPos + 1;
         }
-        //делаем pivot row
+        // calculating new pivot row
         for (int i = 0; i < n; i++) {
-            simplexMatrix[minRatioPos][i] = getWithAccuracy(simplexMatrix[minRatioPos][i] / minRatioVal, accuracy);
+            simplexMatrix[minRatioPos][i] = getWithAccuracy(
+                simplexMatrix[minRatioPos][i] / minRatioVal, accuracy);
         }
-        //здесь мы херачим операции, чтобы сделать pivot column
+        // obtaining pivot column
         for (int b = 0; b < m; b++) {
-            double mnozhitel = simplexMatrix[b][minPos];
+            double factor = simplexMatrix[b][minPos];
             for (int f = 0; f < n; f++) {
                 if (b != minRatioPos) {
-                    simplexMatrix[b][f] = getWithAccuracy(simplexMatrix[b][f] - mnozhitel * simplexMatrix[minRatioPos][f], accuracy);
+                    simplexMatrix[b][f] = getWithAccuracy(
+                        simplexMatrix[b][f] -
+                            factor * simplexMatrix[minRatioPos][f],
+                        accuracy);
                 }
             }
         }
@@ -206,19 +166,18 @@ map<int, double> simplex(vector<vector<double>> simplexMatrix, long long accurac
     for (int i = 0; i < m; i++) {
         ret[variables[i]] = simplexMatrix[i][n - 1];
     }
-    //возвращаем значение solution, а также значения переменных
+    // return solution and decision variables
     return ret;
 }
 
 int main() {
-    //гОСПОДА, чтобы это работало вам нужно будет сделать инпут основанный на n и m, там ниже показано, что это,
-    // далее тупо заполняете матрицу
     long long accuracy = 0;
     vector<vector<double>> simplexMatrix = getMatrix(accuracy);
     map<int, double> ans = simplex(simplexMatrix, accuracy);
     cout << "The result is: " << ans[0] << '\n';
     cout << "It can be obtained with the following values of variables: \n";
-    for (int i = 1; i < simplexMatrix[0].size() - simplexMatrix.size() + 1; i++) {
+    for (int i = 1; i < simplexMatrix[0].size() - simplexMatrix.size() + 1;
+         i++) {
         cout << "x" << i << " = " << ans[i] << '\n';
     }
     return 0;
