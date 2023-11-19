0ded88af6502a7c827e5a43b25bc8f31a361b94b
Iurii <i.podkorytov@innopolis.university>
2023-11-19 11:29:07 +0300

    added comments

--- transportation-problem/transportation.py
+++ transportation-problem/transportation.py
@@ -2,10 +2,10 @@ import numpy as np
 import math
 import copy
 import warnings
-warnings.filterwarnings("ignore", category=RuntimeWarning) # surpressing All-NaN axis encountered which is fine for our purposes. 
+warnings.filterwarnings("ignore", category=RuntimeWarning) # surpressing All-NaN axis encountered
 
-n = 3  # amount of rows of table
-m = 4  # amount of columns of table
+n = 3  # amount of rows of table (sources)
+m = 4  # amount of columns of table (destinations)
 
 
 class MethodException(Exception):
@@ -37,7 +37,7 @@ class Table:
                         raise MethodException()
 
     def print(self):
-        print("--- Parameter Table of Transportation Problem ---")
+        print("Parameter Table of the Transportation Problem")
         for i in range(self.__n):
             for j in range(self.__m):
                 print("{:<10}".format(self.cost[i][j]), end=' ')
@@ -47,7 +47,8 @@ class Table:
             print("{:<10}".format(self.demand[j]), end=' ')
         print()
     
-    def update(self):
+    def update_cost(self):
+        # replaces costs with Nan if the corresponding supply or demand is 0
         for i, s in enumerate(self.supply):
             if s == 0:
                 for j in range(len(self.demand)):
@@ -60,6 +61,7 @@ class Table:
 
 
 def north_west_method(_table):
+    # retuns starting solution obtained with North-west corner method and vector x0
     table = copy.deepcopy(_table)
     pos_x = 0
     pos_y = 0
@@ -81,7 +83,9 @@ def north_west_method(_table):
 
 
 def vogel_method(_table):
+    # retuns starting solution obtained with Vogel's method and vector x0
     def get_delta(arr):
+        # returns the difference between two smallest values in the array
         first = second = math.inf
         for i in range(0, len(arr)):
             if arr[i] < first:
@@ -97,11 +101,13 @@ def vogel_method(_table):
     x0 = []
     while sum(table.supply) > 0 or sum(table.demand) > 0:
         table_cost_transposed = np.array(table.cost).T.tolist()
+        # find rows and colums deltas
         row_difs = list(map(get_delta, table.cost))
         col_difs = list(map(get_delta, table_cost_transposed))
         row_dif = np.nanmax(row_difs)
         col_dif = np.nanmax(col_difs)
          
+        # find the cost corresponding to the maximum delta
         if row_dif >= col_dif:
             i = row_difs.index(row_dif)
             j = table.cost[i].index(np.nanmin(table.cost[i]))
@@ -109,26 +115,31 @@ def vogel_method(_table):
             j = col_difs.index(col_dif)
             i = table_cost_transposed[j].index(np.nanmin(table_cost_transposed[j]))
 
+        # increase the resulting cost and save variable in x0
         val = min(table.supply[i], table.demand[j])
         x0.append((i, j, val))
         res += table.cost[i][j] * val
+
+        # update the table
         table.supply[i] -= val
         table.demand[j] -= val
-        table.update()
+        table.update_cost()
 
     return res, x0
 
 
 def russel_method(_table):
+    # retuns starting solution obtained with Russel's method and vector x0
     table = copy.deepcopy(_table)
     res = 0
     x0 = []
     while sum(table.supply) > 0 or sum(table.demand) > 0:
-        table_cost_transposed = np.array(table.cost).T.tolist()
+        table_cost_transposed = np.array(table.cost).T.tolist() # get transposed table to itterate through columns
         row_maxs = list(map(np.nanmax, table.cost))
         col_maxs = list(map(np.nanmax, table_cost_transposed))
         min_delta = math.inf
         mi, mj = 0, 0
+        # find the most negative delta and the corresponding variable
         for i in range(len(table.supply)):
             for j in range(len(table.demand)):
                 delta = table.cost[i][j] - row_maxs[i] - col_maxs[j]
@@ -136,12 +147,15 @@ def russel_method(_table):
                     min_delta = delta
                     mi, mj = i, j
 
+        # increase the resulting cost and save variable in x0
         val = min(table.supply[mi], table.demand[mj])
         x0.append((mi, mj, val))
         res += table.cost[mi][mj] * val
+
+        # update the table
         table.supply[mi] -= val
         table.demand[mj] -= val
-        table.update()
+        table.update_cost()
 
     return res, x0
 
@@ -151,17 +165,19 @@ def print_result(res):
     print(*(f"x{i}{j} = {e}" for i, j, e in x0), sep = ', ')
 
 if __name__ == '__main__':
+    # EXAMPLE
     # supply_coef = [7, 9, 18]
     # cost_matrix = [[19, 30, 50, 10],
     #                [70, 30, 40, 60],
     #                [40, 8, 70, 20]]
     # demand_coef = [5, 8, 7, 14]
 
+    # input
     print("Enter a vector of supply coefficients, 3 numbers devided by space:")
     supply_coef = list(map(int, input().split()))
     print("Enter a matrix of cost coefficients, 3 row by 4 numbers devived by space:")
     cost_matrix = []
-    for i in range(n):
+    for _ in range(n):
         cost_matrix.append(list(map(int, input().split())))
     print("Enter a vector of coefficients of demand, 4 numbers:")
     demand_coef = list(map(int, input().split()))
@@ -170,13 +186,13 @@ if __name__ == '__main__':
         table = Table(supply_coef, cost_matrix, demand_coef)
         table.print()
 
+        # run the algorithms
         print("\nNorth-west method")
         print_result(north_west_method(table))
         print("\nVogel's method")
         print_result(vogel_method(table))
         print("\nRussel's method")
         print_result(russel_method(table))
-
 
     except BalanceProblem as e:
         print(e)
